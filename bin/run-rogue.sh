fdce25424698de5180552485b113f7909e854418
jacob <jacob@amazon.com>
2019-07-10 14:19:30 -0700

    print more informative messages on startup

--- bin/run-rogue.sh
+++ bin/run-rogue.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+PORT=4099
+echo "To debug use M-x slime-connect to port $PORT, in Emacs"
+
 parent_dir=$(dirname $(readlink -f $(dirname "$0")))"/"
-echo $parent_dir
-sbcl <<EOF
-(ql:quickload "cl-ncurses")
+rogue=$(mktemp -t rogueXXXXXXX.lisp)
+cat > $rogue <<EOF
+(ql:quickload "cl-charms")
+(ql:quickload "swank")
+(swank:create-server :port $PORT)
 (pushnew (merge-pathnames "$parent_dir" (user-homedir-pathname)) asdf:*central-registry*)
+(format t "~&Added $parent_dir to asdf:*central-registry*")
 (asdf:operate 'asdf:load-op 'cl-rogue)
+(format t "~&To debug, use M-x slime-connect to port $PORT, in Emacs
+Hit RETURN to continue")
+(read-line)
 (cl-rogue:rogue)
 EOF
+
+# SBCL seems to turn off input (or something like that) if you don't
+# use the --load argument.
+sbcl --load $rogue
